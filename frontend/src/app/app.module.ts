306930f0fbb490bece4b19343581ee635a77e795
Laura <lcmm97.lcmm@gmail.com>
2023-10-08 19:22:21 -0500

    changes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FiltroComponent } from './filtro/filtro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FiltroComponent
   ],
   imports: [
     BrowserModule,
